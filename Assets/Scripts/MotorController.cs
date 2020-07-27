b006083f22489d1a984790946d830ca0819c420f
Yuan Li <vanish8.7@gmail.com>
2020-07-27 19:41:16 +0900

    add water force and objective functions

--- Assets/Scripts/MotorController.cs
+++ Assets/Scripts/MotorController.cs
@@ -1,28 +1,283 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityTools.Common;
+using UnityTools.Debuging.EditorTool;
 
 namespace UnityFishSimulation
 {
     public class MotorController : MonoBehaviour
     {
-        public enum Type
+        [System.Serializable]
+        public class MuscleActuatorControlFunction
         {
-            Swim,
-            Balance,
-        }
-        public class MuscleActuation
-        {
-            public List<float2> valueMap;
-            public float Evaluate(float t)
+            [SerializeField] protected CricleData<List<float2>, int> valueMap;
+            public MuscleActuatorControlFunction(float start, float h, int sampleSize)
             {
-                //Temp just return sin value
-                return math.sin(t);
+                this.valueMap = new CricleData<List<float2>, int>(2);
+                for(var i = 0; i < sampleSize; ++i)
+                {
+                    this.valueMap.Current.Add(new float2(start + h * i, UnityEngine.Random.value));
+                    this.valueMap.Next.Add(new float2(start + h * i, UnityEngine.Random.value));
+                }
+            }
+            public void RandomNextValues()
+            {
+                for (var i = 0; i < this.valueMap.Next.Count; ++i)
+                {
+                    this.valueMap.Next[i] = new float2(this.valueMap.Next[i].x, UnityEngine.Random.value);
+                }
+            }
+            public void MoveToNext()
+            {
+                this.valueMap.MoveToNext();
+            }
+            public void MoveToPrev()
+            {
+                this.valueMap.MoveToPrev();
+            }
+            public float Evaluate(float t, float2 minMax)
+            {
+                var range = minMax.y - minMax.x;
+                var h = range / this.valueMap.Current.Count;
+
+                var index = (t % minMax.y) / h;
+                var from = Mathf.FloorToInt(index);
+                var to = Mathf.CeilToInt(index);
+                var yfrom = this.Evaluate(from);
+                var yto = this.Evaluate(to);
+
+                return math.lerp(yfrom, yto, index-from);
+            }
+
+            public float Evaluate(int index)
+            {
+                var x = math.clamp(index, 0, this.valueMap.Current.Count - 1);
+                return this.valueMap.Current[x].y;
+            }
+
+            public float Devrivate(int index, float h)
+            {
+                return (this.Evaluate(index - 1) + this.Evaluate(index + 1)) / (2 * h);
+            }
+            public float Devrivate2(int index, float h)
+            {
+                return (this.Evaluate(index - 1) + this.Evaluate(index + 1) - (2 * this.Evaluate(index))) / (h * h);
+            }
+            public void OnGizmos()
+            {
+                if (this.valueMap != null)
+                {
+                    using (new GizmosScope(Color.cyan, Matrix4x4.TRS(new Vector3(10, 0, 0), Quaternion.identity, Vector3.one)))
+                    {
+                        for (var i = 1; i < this.valueMap.Current.Count; ++i)
+                        {
+                            var from = new Vector3(this.valueMap.Current[i - 1].x, this.valueMap.Current[i - 1].y);
+                            var to = new Vector3(this.valueMap.Current[i].x, this.valueMap.Current[i].y);
+                            Gizmos.DrawLine(from, to);
+                        }
+                    }
+                }
             }
         }
 
+        public const float Smax = 0.075f;
+
         [SerializeField] protected StructureModel fishModel;
+        [SerializeField]
+        protected List<float2> amplitudeParameter = new List<float2>(2)
+        {
+            new float2(0,1),
+            new float2(0,1),
+        };
+        [SerializeField]
+        protected List<float2> frequencyParameter = new List<float2>(2)
+        {
+            new float2(0,Smax),
+            new float2(0,Smax),
+        };
+
+        [SerializeField] protected List<MuscleActuatorControlFunction> activations = new List<MuscleActuatorControlFunction>();
+
+        [SerializeField] protected float2 timeInterval = new float2(0, 5);
+        [SerializeField] protected int sampleSize = 15;
+        [SerializeField] protected float h;
+
+        [SerializeField] protected float temperature = 1;
+        [SerializeField] protected float minTemperature = 0.0001f;
+        [SerializeField] protected float alpha = 0.98f;
+
+        protected void Start()
+        {
+            var num = 3;// 12;
+            this.activations.Clear();
+
+            var start = this.timeInterval.x;
+            this.h = (this.timeInterval.y - this.timeInterval.x) / sampleSize;
+
+            for (var i = 0; i < num; ++i)
+            {
+                this.activations.Add(new MuscleActuatorControlFunction(start, this.h, this.sampleSize));
+            }
+
+        }
+
+        protected void Update()
+        {
+            //if(Input.GetKey(KeyCode.N))
+            {
+                this.StepSimulatedAnnealing();
+            }
+
+            if(Input.GetKey(KeyCode.M))
+            {
+                this.ApplyToMuscle();
+            }
+        }
+
+        protected void ApplyByType(StructureModel.Spring.Type type)
+        {
+            var muscle = this.fishModel.GetSpringByType(type);
+            var muscleLeft = muscle.Where(s => s.side == StructureModel.Spring.Side.Left);
+            var muscleRight = muscle.Where(s => s.side == StructureModel.Spring.Side.Right);
+            var activation = this.activations[0];
+
+
+            this.testX += 0.005f;
+            this.testX %= this.timeInterval.y;
+
+            var t = this.testX;
+            t = (t + (type == StructureModel.Spring.Type.MuscleBack ? math.PI : 0)) % this.timeInterval.y;
+
+
+            foreach (var l in muscleLeft)
+            {
+                //l.activation = (math.sin(t) + 1) * 0.5f;// 
+                l.activation = activation.Evaluate(t, this.timeInterval);
+            }
+            foreach (var r in muscleRight)
+            {
+                //r.activation = 1 - (math.sin(t) + 1) * 0.5f;// 
+                r.activation = 1 - activation.Evaluate(t, this.timeInterval);
+            }
+
+        }
+
+        protected void ApplyToMuscle()
+        {
+            this.ApplyByType(StructureModel.Spring.Type.MuscleBack);
+            this.ApplyByType(StructureModel.Spring.Type.MuscleMiddle);
+        }
+
+        protected void StepSimulatedAnnealing()
+        {
+            var count = 0;
+            var current = this.GetCurrentE();
+            while (this.temperature > this.minTemperature)
+            {
+                count++;
+                foreach (var a in this.activations)
+                {
+                    a.RandomNextValues();
+                    a.MoveToNext();
+                }
+
+                var next = this.GetCurrentE();
+                if (this.ShouldUseNext(current, next) == false)
+                {
+                    foreach (var a in this.activations)
+                    {
+                        a.MoveToPrev();
+                    }
+                }
+
+                //this.temperature = this.temperature/(1+this.alpha*this.temperature);
+                this.temperature *= this.alpha;
+
+                //Debug.Log("Current " + current + " Next " + next);
+            }
+            
+            {
+                //Debug.Log("Current " + current);
+                //Debug.Log("Min temp reached with count " + count);
+                if(current > 1) this.temperature = 1;
+            }
+
+        }
+
+        protected bool ShouldUseNext(float current, float next)
+        {
+            if (next < current)
+            {
+                return true;
+            }
+            else
+            {
+                var p = math.pow(math.E, -(next - current) / this.temperature);
+                if (p > UnityEngine.Random.value)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        protected float GetCurrentE()
+        {
+            var mu1 = 1f;
+            var mu2 = 0f;
+
+            var E = 0f;
+
+            for (int i = 0; i < this.sampleSize; ++i)
+            {
+                var Eu = 0f;
+                var Ev = 0f;
+
+                var v1 = 1f;
+                var v2 = 1f;
+
+                var du = 0f;
+                var du2 = 0f;
+                foreach (var fun in this.activations)
+                {
+                    var dev = fun.Devrivate(i, this.h);
+                    var dev2 = fun.Devrivate2(i, this.h);
+                    du += dev * dev;
+                    du2 += dev2 * dev2;
+                }
+
+                Eu = 0.5f * (v1 * du + v2 * du2);
+
+                E += mu1 * Eu + mu2 * Ev;
+            }
+
+            return E;
+        }
+
+        public float testX = 0;
+        public float testY = 0;
+        protected void OnDrawGizmos()
+        {
+            if (this.activations.Count > 0)
+            {
+                this.activations[0].OnGizmos();
+
+                testY = this.activations[0].Evaluate(testX, this.timeInterval);
+                Gizmos.DrawSphere(new Vector3(testX+10, testY), 0.1f);
+            }
+        }
+
+    }
+
+    public class SwimMotorController : MotorController
+    {
+        [SerializeField] protected float speed;
 
 
     }
--- Assets/Scripts/StructureModel.cs
+++ Assets/Scripts/StructureModel.cs
@@ -5,6 +5,7 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityTools.Common;
+using UnityTools.Debuging.EditorTool;
 
 namespace UnityFishSimulation
 {
@@ -38,6 +39,13 @@ namespace UnityFishSimulation
                 Normal,
             }
 
+            public enum Side
+            {
+                Left,
+                Right,
+                None,
+            }
+
             protected Dictionary<Type, float> elasticMap = new Dictionary<Type, float>()
             {
                 {Type.Cross , 38f },
@@ -49,18 +57,124 @@ namespace UnityFishSimulation
 
             public float c = 38;  // elasticity constant
             public float k = 0.1f;// viscosity constant
-            public float l = 1;   // rest length
+            public float lr = 1;   // rest length
+            public float lc = 1;   // fully contracted length
+            public float activation = 0;
             public Type type = Type.Normal;
+            public Side side = Side.None;
 
-            public Spring(Type type)
+            public Spring(Type type, Side side)
             {
                 this.type = type;
+                this.side = side;
                 this.c = elasticMap[this.type];
             }
+
+            public float CurrentL { get => math.lerp(this.lr, this.lc, this.activation); }
 
             public override string ToString()
             {
                 return "1";
+            }
+        }
+
+        [System.Serializable]
+        public class NormalFace
+        {
+            [SerializeField] protected float3 normal;
+            public List<MassPoint> nodeList = new List<MassPoint>();
+
+            public float3 Normal
+            {
+                get
+                {
+                    this.CalNormal();
+                    return this.normal;
+                }
+            }
+            public NormalFace(MassPoint p1, MassPoint p2, MassPoint p3, MassPoint p4)
+            {
+                this.nodeList.Add(p1);
+                this.nodeList.Add(p2);
+                this.nodeList.Add(p3);
+                if (p4 != null) this.nodeList.Add(p4);
+            }
+            public void OnGizmos(float length = 1)
+            {
+                var p1 = this.nodeList[0].Position;
+                var p2 = this.nodeList[1].Position;
+                var p3 = this.nodeList[2].Position;
+                var p4 = this.nodeList.Count > 3 ? this.nodeList[3].Position : float3.zero;
+                var mid = (p1+p2+p3+p4)/ this.nodeList.Count;
+                using (new GizmosScope(Color.yellow, Matrix4x4.identity))
+                {
+                    Gizmos.DrawLine(mid, mid + this.Normal * length);
+                }
+            }
+
+            public void ApplyForceToNode(float mu = 1)
+            {
+                var area = this.CalArea();
+                var velocity = this.CalVelocity();
+                var n = this.Normal;
+                var force = -mu * area * math.length(velocity) * (math.dot(n, velocity) * n);
+                force = math.min(0, force);
+
+                var num = this.nodeList.Count;
+                force /= num;
+
+                foreach(var node in this.nodeList)
+                {
+                    node.force += force;
+                }
+            }
+
+            protected void CalNormal()
+            {
+                var p1 = this.nodeList[0].Position;
+                var p2 = this.nodeList[1].Position;
+                var p3 = this.nodeList[2].Position;
+                var v1 = p2 - p1;
+                var v2 = p3 - p1;
+
+                this.normal = math.normalize(math.cross(v2, v1));
+            }
+
+            protected float Area(float3 p1, float3 p2, float3 p3)
+            {
+                var v1 = p2 - p1;
+                var v2 = p3 - p1;
+                var cos = math.dot(math.normalize(v1), math.normalize(v2));
+                var sin = math.sqrt(1 - cos * cos);
+                return math.length(v1) * math.length(v2) * sin;
+            }
+
+            protected float CalArea()
+            {
+                var p1 = this.nodeList[0].Position;
+                var p2 = this.nodeList[1].Position;
+                var p3 = this.nodeList[2].Position;
+                var p4 = this.nodeList.Count > 3 ? this.nodeList[3].Position : float3.zero;
+                var num = this.nodeList.Count;
+                if(num == 3)
+                {
+                    return this.Area(p1, p2, p3);
+                }
+                else
+                {
+                    Assert.IsTrue(num == 4);
+                    return this.Area(p1, p2, p4) + this.Area(p3, p4, p2);
+                }
+            }
+            protected float3 CalVelocity()
+            {
+                var v1 = this.nodeList[0].velocity;
+                var v2 = this.nodeList[1].velocity;
+                var v3 = this.nodeList[2].velocity;
+                var v4 = this.nodeList.Count > 3 ? this.nodeList[3].velocity : float3.zero;
+                var num = this.nodeList.Count;
+
+                return (v1 + v2 + v3 + v4) / num;
             }
         }
 
@@ -76,8 +190,29 @@ namespace UnityFishSimulation
         };
 
         protected Graph<MassPoint, Spring> fishGraph = new Graph<MassPoint, Spring>(23);
+        [SerializeField] protected List<NormalFace> normals = new List<NormalFace>();
+
         [SerializeField] protected List<MassPoint> runtimeList;
+        [SerializeField] protected List<Spring> runtimeMuscleList;
         [SerializeField] protected List<Spring> runtimeSpringList;
+
+        public List<Spring> GetSpringByType(Spring.Type type)
+        {
+            var ret = new List<Spring>();
+            for (var r = 0; r < this.fishGraph.AdjMatrix.Size.x; ++r)
+            {
+                for (var c = 0; c < this.fishGraph.AdjMatrix.Size.y; ++c)
+                {
+                    var s = this.fishGraph.GetEdge(r, c);
+                    if (s == null || s.type != type) continue;
+                    if (ret.Contains(s)) continue;
+
+                    ret.Add(s);
+                }
+            }
+
+            return ret;
+        }
 
         protected void Start()
         {
@@ -86,8 +221,8 @@ namespace UnityFishSimulation
 
             this.runtimeList = this.fishGraph.Nodes.ToList();
 
-            //this.fishGraph.AdjMatrix.Clean();
-            //this.InitSprings();
+            this.fishGraph.AdjMatrix.Clean();
+            this.InitSprings();
 
             this.runtimeSpringList = new List<Spring>();
             for (var r = 0; r < this.fishGraph.AdjMatrix.Size.x; ++r)
@@ -101,7 +236,53 @@ namespace UnityFishSimulation
                     this.runtimeSpringList.Add(edge);
                 }
             }
+
+            this.runtimeMuscleList = this.GetSpringByType(Spring.Type.MuscleBack);
+            this.runtimeMuscleList.AddRange(this.GetSpringByType(Spring.Type.MuscleMiddle));
+            this.runtimeMuscleList.AddRange(this.GetSpringByType(Spring.Type.MuscleFront));
+
+            this.InitNormals();
         }        
+
+        protected void InitNormals()
+        {
+            this.AddNormalFace(0, 1, 2);
+            this.AddNormalFace(0, 2, 3);
+            this.AddNormalFace(0, 3, 4);
+            this.AddNormalFace(0, 4, 1);
+
+            this.AddNormalFace(1, 5, 6, 2);
+            this.AddNormalFace(2, 6, 7, 3);
+            this.AddNormalFace(3, 7, 8, 4);
+            this.AddNormalFace(4, 8, 5, 1);
+
+            this.AddNormalFace(5, 9, 10, 6);
+            this.AddNormalFace(6, 10, 11, 7);
+            this.AddNormalFace(7, 11, 12, 8);
+            this.AddNormalFace(8, 12, 9, 5);
+
+            this.AddNormalFace(9, 13, 14, 10);
+            this.AddNormalFace(10, 14, 15, 11);
+            this.AddNormalFace(11, 15, 16, 12);
+            this.AddNormalFace(12, 16, 13, 9);
+
+
+            this.AddNormalFace(13, 17, 18, 14);
+            this.AddNormalFace(14, 18, 19, 15);
+            this.AddNormalFace(15, 19, 20, 16);
+            this.AddNormalFace(16, 20, 17, 13);
+
+            this.AddNormalFace(17, 21, 18);
+            this.AddNormalFace(18, 21, 22, 19);
+            this.AddNormalFace(19, 22, 20);
+            this.AddNormalFace(20, 22, 21, 17);
+        }
+
+        protected void AddNormalFace(int p1, int p2, int p3, int p4 = -1)
+        {
+            var nodes = this.fishGraph.Nodes.ToList();
+            this.normals.Add(new NormalFace(nodes[p1], nodes[p2], nodes[p3], p4<0?null:nodes[p4]));
+        }
 
         protected void InitSprings()
         {
@@ -113,70 +294,70 @@ namespace UnityFishSimulation
             this.AddSpring(1, 2, Spring.Type.Normal);
             this.AddSpring(1, 3, Spring.Type.Cross);
             this.AddSpring(1, 4, Spring.Type.Normal);
-            this.AddSpring(1, 5, Spring.Type.MuscleFront);//
+            this.AddSpring(1, 5, Spring.Type.MuscleFront, Spring.Side.Left);//
             this.AddSpring(1, 6, Spring.Type.Cross);
             this.AddSpring(1, 8, Spring.Type.Cross);
 
             this.AddSpring(2, 3, Spring.Type.Normal);
             this.AddSpring(2, 4, Spring.Type.Cross);
             this.AddSpring(2, 5, Spring.Type.Cross);
-            this.AddSpring(2, 6, Spring.Type.MuscleFront);//
+            this.AddSpring(2, 6, Spring.Type.MuscleFront, Spring.Side.Right);//
             this.AddSpring(2, 7, Spring.Type.Cross);
 
             this.AddSpring(3, 4, Spring.Type.Normal);
             this.AddSpring(3, 6, Spring.Type.Cross);
-            this.AddSpring(3, 7, Spring.Type.MuscleFront);//
+            this.AddSpring(3, 7, Spring.Type.MuscleFront, Spring.Side.Right);//
             this.AddSpring(3, 8, Spring.Type.Cross);
 
             this.AddSpring(4, 5, Spring.Type.Cross);
             this.AddSpring(4, 7, Spring.Type.Cross);
-            this.AddSpring(4, 8, Spring.Type.MuscleFront);//
+            this.AddSpring(4, 8, Spring.Type.MuscleFront, Spring.Side.Left);//
             //---------------------------------
 
             this.AddSpring(5, 6, Spring.Type.Normal);
             this.AddSpring(5, 7, Spring.Type.Cross);
             this.AddSpring(5, 8, Spring.Type.Normal);
-            this.AddSpring(5, 9, Spring.Type.MuscleMiddle);
+            this.AddSpring(5, 9, Spring.Type.MuscleMiddle, Spring.Side.Left);
             this.AddSpring(5, 10, Spring.Type.Cross);
             this.AddSpring(5, 12, Spring.Type.Cross);
 
             this.AddSpring(6, 7, Spring.Type.Normal);
             this.AddSpring(6, 8, Spring.Type.Cross);
             this.AddSpring(6, 9, Spring.Type.Cross);
-            this.AddSpring(6, 10, Spring.Type.MuscleMiddle);
+            this.AddSpring(6, 10, Spring.Type.MuscleMiddle, Spring.Side.Right);
             this.AddSpring(6, 11, Spring.Type.Cross);
 
             this.AddSpring(7, 8, Spring.Type.Normal);
             this.AddSpring(7, 10, Spring.Type.Cross);
-            this.AddSpring(7, 11, Spring.Type.MuscleMiddle);
+            this.AddSpring(7, 11, Spring.Type.MuscleMiddle, Spring.Side.Right);
             this.AddSpring(7, 12, Spring.Type.Cross);
 
             this.AddSpring(8, 9, Spring.Type.Cross);
             this.AddSpring(8, 11, Spring.Type.Cross);
-            this.AddSpring(8, 12, Spring.Type.MuscleMiddle);
+            this.AddSpring(8, 12, Spring.Type.MuscleMiddle, Spring.Side.Left);
             //---------------------------------
 
             this.AddSpring(9, 10, Spring.Type.Normal);
             this.AddSpring(9, 11, Spring.Type.Cross);
             this.AddSpring(9, 12, Spring.Type.Normal);
-            this.AddSpring(9, 13, Spring.Type.MuscleBack);
+            this.AddSpring(9, 13, Spring.Type.MuscleBack, Spring.Side.Left);
             this.AddSpring(9, 14, Spring.Type.Cross);
             this.AddSpring(9, 16, Spring.Type.Cross);
 
             this.AddSpring(10, 11, Spring.Type.Normal);
             this.AddSpring(10, 12, Spring.Type.Cross);
             this.AddSpring(10, 13, Spring.Type.Cross);
-            this.AddSpring(10, 14, Spring.Type.MuscleBack);
+            this.AddSpring(10, 14, Spring.Type.MuscleBack, Spring.Side.Right);
             this.AddSpring(10, 15, Spring.Type.Cross);
 
             this.AddSpring(11, 12, Spring.Type.Normal);
             this.AddSpring(11, 14, Spring.Type.Cross);
-            this.AddSpring(11, 15, Spring.Type.MuscleBack);
+            this.AddSpring(11, 15, Spring.Type.MuscleBack, Spring.Side.Right);
             this.AddSpring(11, 16, Spring.Type.Cross);
 
             this.AddSpring(12, 13, Spring.Type.Cross);
             this.AddSpring(12, 15, Spring.Type.Cross);
-            this.AddSpring(12, 16, Spring.Type.MuscleBack);
+            this.AddSpring(12, 16, Spring.Type.MuscleBack, Spring.Side.Left);
             //--------------------------------
 
             this.AddSpring(13, 14, Spring.Type.Normal);
@@ -224,15 +405,16 @@ namespace UnityFishSimulation
 
         }
 
-        protected void AddSpring(int from, int to, Spring.Type type, float k = 0.1f)
+        protected void AddSpring(int from, int to, Spring.Type type, Spring.Side side = Spring.Side.None, float k = 0.1f)
         {
             var nodes = this.fishGraph.Nodes.ToList();
-            var s = new Spring(type);
+            var s = new Spring(type, side);
             s.Left = nodes[from];
             s.Right = nodes[to];
 
             s.k = k;
-            s.l = math.length(nodes[from].Position - nodes[to].Position);
+            s.lr = math.length(nodes[from].Position - nodes[to].Position);
+            s.lc = s.lr * 0.6f;
 
             this.fishGraph.AddEdge(from, to, s);
         }
@@ -250,7 +432,7 @@ namespace UnityFishSimulation
                 //this.runtimeList = this.fishGraph.Nodes.ToList();
             }
 
-            if(Input.GetKey(KeyCode.G))
+            //if(Input.GetKey(KeyCode.G))
             {
                 this.Step();
             }
@@ -280,6 +462,8 @@ namespace UnityFishSimulation
                 }
             }
             foreach (var n in this.fishGraph.Nodes) n.OnGizmos(50 * Unit.WorldMMToUnityUnit);
+
+            foreach (var n in this.normals) n.OnGizmos(200 * Unit.WorldMMToUnityUnit);
         }
 
         protected void Step()
@@ -291,7 +475,9 @@ namespace UnityFishSimulation
                 n.force = force;
             }
 
-            foreach(var n in this.fishGraph.Nodes)
+            this.ApplyFluidForce();
+
+            foreach (var n in this.fishGraph.Nodes)
             {
                 n.velocity += n.force * dt;
                 n.Position += n.velocity * dt;
@@ -309,7 +495,7 @@ namespace UnityFishSimulation
                 var s_ij = this.fishGraph.GetEdge(i, j);
                 Assert.IsNotNull(s_ij);
 
-                var e_ij = r_ij - s_ij.l;
+                var e_ij = r_ij - s_ij.CurrentL;
 
                 var u_ij = j.velocity - i.velocity;
                 var r_dot = (u_ij * r) / r_ij;
@@ -319,6 +505,14 @@ namespace UnityFishSimulation
             }
 
             return ret;
+        }
+
+        protected void ApplyFluidForce()
+        {
+            foreach(var face in this.normals)
+            {
+                face.ApplyForceToNode();
+            }
         }
 
     }
